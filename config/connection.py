5081405c423368293785eb7ef0e73d62ce759e8f
harveybia <harvey.shi@yahoo.com>
2016-02-09 17:04:38 -0500

    Framework Major Update, TCP/IP Protocol

--- config/connection.py
+++ config/connection.py
@@ -1,1 +1,53 @@
 import rpyc
+import socket
+import cv2
+import numpy as np
+import threading
+
+# My Cam Listener TCP addr
+TCP_IP = socket.gethostname()
+TCP_PORT = 15122
+terminated = False
+transmitting = False
+
+# Server addr, should be raspi
+ADDR, PORT = "128.237.141.170", 15251
+
+def recvall(sock, count):
+    buf = b''
+    while count:
+        newbuf = sock.recv(count)
+        if not newbuf: return None
+        buf += newbuf
+        count -= len(newbuf)
+    return buf
+
+def listenToCam():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
+    print "listening to %d:%d"%(TCP_IP, TCP_PORT)
+    s.listen(True)
+    conn, addr = s.accept()
+    transmitting = True
+    print "accepted connection from %s"%addr
+
+    length = recvall(conn, 16)
+    stringData = recvall(conn, int(length))
+
+    data = np.fromstring(stringData, dtype='uint8')
+    img = cv2.imdecode(data, 1)
+
+    transmitting = False
+    # Test Code
+    cv2.imshow('Server', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+while not terminated:
+    c = rpyc.connect(ADDR, PORT)
+    c.root.configureVideoStream(TCP_IP, TCP_PORT)
+    while transmitting:
+        pass
+    # Close connection
+    time.sleep(2)
+    s.close()
--- mobot/server.py
+++ mobot/server.py
@@ -1,19 +1,61 @@
 import logging
 import rpyc
+import time
 import cv2
+import io
+import numpy as np
+import picamera
+import easyterm
+import socket
+import rpyc
 from rpyc.utils.server import ThreadedServer
+
+# This is the mobot Backend Service
+# Can expose service locally for algorithmic clients to send Instructions
+
+# IP Address for camera data stream host target
+TCP_IP = '128.237.138.121'
+TCP_PORT = 15122
+STREAM = io.BytesIO()
+CAMERA = picamera.PiCamera()
+
+sock = socket.socket()
+sock.connect((TCP_IP, TCP_PORT))
+
+term = easyterm.TerminalController()
+
+def info(msg):
+    print time.ctime(),
+        " INFO: ", term.render("${YELLOW}%s${NORMAL}"%msg)
+
+def warn(msg):
+    print time.ctime(),
+        " WARN: ", term.render("${RED}${BG_WHITE}%s${NORMAL}"%msg)
 
 class MobotScv(rpyc.Service):
     def __init__(self, *args, **kwargs):
         rpyc.Service.__init__(self, *args, **kwargs)
-        self.cameraCallback = None
+        info("starting camera preview, salibrating Color")
+        CAMERA.start_preview()
+        info("preview complete, capturing footage into stream")
+        CAMERA.capture(STREAM, format='jpeg', resize=(320, 240))
+
+    @staticmethod
+    def getCameraSnapshot():
+        # Returns the snapshot array with BGR in OpenCV nparray format
+        data = np.fromstring(STREAM.getvalue(), dtype=np.utf8)
+        return cv2.imdecode(data, 1)
 
     def on_connect(self):
-        print "Incoming Conn"
-        logging.info("Incoming Connection")
+        info("connection established")
 
     def on_disconnect(self):
-        logging.info("Connection Closed")
+        info("connection lost")
+
+    def exposed_configureVideoStream(self, addr, port):
+        TCP_IP = addr
+        TCP_PORT = port
+        info("configured cam stream addr: %s, port: %d"%(TCP_IP, TCP_PORT))
 
     def exposed_getBattery(self):
         return 100
@@ -23,19 +65,38 @@ class MobotScv(rpyc.Service):
 
     def exposed_getCameraConfig(self):
         return {
-                "CAMERA_ENABLED": True,
-                "CAMERA_X_OFFSET": 10,
-                "CAMERA_Y_OFFSET": -10,
-                "CAMERA_MOUNT_ANGLE": 45
-                }
-
-    def exposed_setCameraCallback(self, func):
-        self.cameraCallback = func
+            "CAMERA_ENABLED": True,
+            "CAMERA_X_OFFSET": 10,
+            "CAMERA_Y_OFFSET": -10,
+            "CAMERA_MOUNT_ANGLE": 45
+            }
 
     def exposed_getCameraSnapshot(self):
-        # Function to return the snapshot taken by camera, in cv2 BGR list form.
+        # This function will not return any value
+        # It invokes a signal to send snapshot via TCP/IP Protocol
+        # It will probably block the main thread
         # GrayScale = True
-        return cv2.imread('../tests/1.JPG',0)
+        info("cam snapshot requested")
+        img = MobotScv.getCameraSnapshot()
+
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
+        result, imgencode = cv2.imencode('.jpg', img, encode_param)
+        data = np.array(imgencode)
+        stringData = data.tostring()
+
+        try:
+            sock.send(str(len(stringData)).ljust(16))
+            sock.send(stringData)
+            info("snapshot sent successfully")
+        except socket.timeout:
+            warn("connection timed out, plesae check listener status")
+            print "detailed Report:"
+            print term.render("${RED}IP_ADDR: ${GREEN}%s${NORMAL}"%TCP_IP)
+            print term.render("${RED}PORT: : ${GREEN}%d${NORMAL}"%TCP_PORT)
+            print term.render("${RED}connection timed out after %.3f seconds \
+                ${NORMAL}" %sock.gettimeout())
+        info("closing socket")
+        sock.close()
 
 if __name__ == "__main__":
     server = ThreadedServer(MobotScv, port = 15251)
