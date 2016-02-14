6f4e15d497e76ac654ef7286688826c41319f32c
harveybia <harvey.shi@yahoo.com>
2016-02-13 23:11:46 -0500

    Simple Stream

--- config/connection.py
+++ config/connection.py
@@ -16,19 +16,20 @@ terminated = False
 
 # Server addr, should be raspi
 ADDR, PORT = "128.237.174.4", 15251
+VIDEO_PORT = 15252
 #ADDR, PORT = "localhost", 15251
 
 if __name__ == "__main__":
     # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
     # all interfaces)
     c = rpyc.connect(ADDR, PORT)
-    c.root.configureVideoStream(TCP_IP, TCP_PORT)
     c.root.startVideoStream_H264()
 
-    video = cv2.VideoCapture('tcp://%s:%d'%(TCP_IP, TCP_PORT))
-    while True:
-        ret, frame = video.read()
-        cv2.imshow("SERVER", frame)
-        if cv2.waitKey(1) == ord('q'):
-            break
-            
+    #video = cv2.VideoCapture('tcp://%s:%d/'%(ADDR, VIDEO_PORT))
+    #while True:
+        #ret, frame = video.read()
+        #print type(frame)
+        #print frame
+        #cv2.imshow("SERVER", frame)
+        #if cv2.waitKey(1) == ord('q'):
+            #break
---
+++ mobot/videostream.py
@@ -0,0 +1,18 @@
+import picamera
+import socket
+
+ADDR, PORT = "", 15252
+
+def startServing():
+    s = socket.socket()
+    s.bind((ADDR, PORT))
+    s.listen(0)
+    cam = picamera.PiCamera()
+    cam.resolution = (320, 240)
+    cam.framerate = 24
+    cam.start_preview()
+    time.sleep(0.5)
+
+    s.listen(0)
+    conn = s.accept()[0].makefile('wb')
+    cam.start_recording(conn, format='h264')
