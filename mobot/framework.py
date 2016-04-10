495720f045a376448b77cca9ed982e4ecc8b1674
harveybia <harvey.shi@yahoo.com>
2016-04-10 15:13:17 -0400

    more p term

--- mobot/framework.py
+++ mobot/framework.py
@@ -597,7 +597,7 @@ class MobotService(rpyc.Service):
         self.basespeed = 100
         self.errorsum = 0
         self.preverr = 0
-        self.p = 3
+        self.p = 5
         self.i = 0
         self.d = 1
 
