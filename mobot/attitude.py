57ea9ec87d741847af23aac90bf8d0c881f6cb14
harveybia <harvey.shi@yahoo.com>
2016-04-14 20:02:12 -0400

    incline test

--- mobot/attitude.py
+++ mobot/attitude.py
@@ -1,22 +1,24 @@
 import time
 from i2clibraries import i2c_hmc5883l
 
-INCLINE_THRESHOLD = 420
-hmc5883l = i2c_hmc5883l.hmc5883l(declination = (-9, -18))
+# A value greater than this will be perceived as inclined down
+INCLINE_THRESHOLD = -20
+MAGNETIC_DECLINATION = (-9, -18)
+
+hmc5883l = i2c_hmc5883l.hmc5883l(declination = MAGNETIC_DECLINATION)
 
 def getMobotAttitude():
     return hmc5883l.axes()
 
 def getMobotInclined():
-    (x,y,z) = getMobotAttitude()
-    inclineVal = abs(y)
-    if abs(inclineVal - INCLINE_THRESHOLD) <= 50:
+    x, y, z = getMobotAttitude()
+    if z > INCLINE_THRESHOLD:
+        return True
+    else:
         return False
-
 
 if __name__ == "__main__":
     while 1:
         print getMobotAttitude()
-        print hmc5883l.heading()
-        print hmc5883l.degrees(0)
+        print getMobotInclined()
         time.sleep(0.5)
